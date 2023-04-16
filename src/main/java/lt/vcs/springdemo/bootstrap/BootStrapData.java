a06af94e64aec9f6ded46336112435bee01ba095
sharas00 <sarunas.pusinskis@gmail.com>
2023-04-16 20:42:08 +0300

    Add Autowired annotation in BootStrapData

--- src/main/java/lt/vcs/springdemo/bootstrap/BootStrapData.java
+++ src/main/java/lt/vcs/springdemo/bootstrap/BootStrapData.java
@@ -2,17 +2,15 @@ package lt.vcs.springdemo.bootstrap;
 
 import lt.vcs.springdemo.repository.CustomerRepository;
 import org.hibernate.annotations.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BootStrapData implements CommandLineRunner {
 
+    @Autowired
     private CustomerRepository customerRepository;
-
-    public BootStrapData(CustomerRepository customerRepository) {
-        this.customerRepository = customerRepository;
-    }
 
     @Override
     public void run(String... args) throws Exception {
